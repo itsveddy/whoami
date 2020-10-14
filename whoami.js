a072a3d0d5024210f42775e4aa2162c8dd9c94ac
Lorenzo Vedana <71873284+itsveddy@users.noreply.github.com>
2020-10-14 20:47:31 +0200

    WhoAmI V1.0

--- whoami.js
+++ whoami.js
@@ -1,14 +1,21 @@
 // 2020 Vedana Lorenzo
 // JavaScript Source Code
 // Available at https://github.com/itsveddy/whoami/
+// V 1.0
 
 function nome() {
   var nome = "Lorenzo";
   var cognome = "Vedana";
-  $("#output").html(`<p>Hello, I'm ${nome} ${cognome}!</p>`);
+  $("#output").append(`<p>Hello, I'm ${nome} ${cognome}!</p>`);
 }
 
 function azione () {
-  var coding = "I'm trying to fly in the computer programming world..."
-  $("#output").append(`<p>${coding}</p>`)
+  var coding = "I'm trying to fly in the computer programming world...";
+  $("#output").append(`<p>${coding}</p>`);
+}
+
+function github () {
+  var github = "GitHub - Who am I";
+  var result = github.link ("https://github.com/itsveddy/whoami/");
+  $("#link").append(`<p>${result}</p>`);
 }
